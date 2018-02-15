117c6425cab73e5695ead2521faec75d9c891bb7
Lee Roden <lrodentx@gmail.com>
2018-02-15 16:07:30 -0600

    fix broken test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -6,7 +6,6 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Component, NgModule } from '@angular/core';
-import { MockUser } from './test/core/services/MockUser';
 import { UserService } from './core/services/user.service';
 import { AngularFireAuth } from 'angularfire2/auth';
 
